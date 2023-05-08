f9be1e28ff9be2604f4b747124beab73972acd89
Andy Jung <Andy.Jung@framegroup.com.au>
2023-05-08 21:09:31 +1000

    Refactor get genres, platforms using ReactQuery

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -10,14 +10,17 @@ import { MdPhoneIphone } from "react-icons/md";
 import { SiNintendo } from "react-icons/si";
 import { BsGlobe } from "react-icons/bs";
 import { Platform } from "../hooks/useGames";
-import { HStack, Icon } from "@chakra-ui/react";
+import { HStack, Icon, Spinner } from "@chakra-ui/react";
 import { IconType } from "react-icons";
+import usePlatforms from "../hooks/usePlatforms";
 
 interface Props {
   platforms: Platform[];
 }
 
 const PlatformIconList = ({ platforms }: Props) => {
+  const { data, isLoading, error } = usePlatforms();
+
   const iconMap: { [key: string]: IconType } = {
     pc: FaWindows,
     playstation: FaPlaystation,
@@ -30,9 +33,12 @@ const PlatformIconList = ({ platforms }: Props) => {
     web: BsGlobe,
   };
 
+  if (error) return null;
+  if (isLoading) return <Spinner />;
+
   return (
     <HStack marginY={1}>
-      {platforms.map((platform) => (
+      {data?.results.map((platform) => (
         <Icon key={platform.id} as={iconMap[platform.slug]} color="gray.500">
           {platform.name}
         </Icon>
